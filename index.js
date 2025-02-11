6e099f876a8a6d30a10eb52705593b115aeb4c54
Yash Rai <157264996+Yashrai54@users.noreply.github.com>
2025-02-11 18:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.getElementById("btn").addEventListener("click",()=>{
-    window.location.replace("https://t.me/c/2498535704/35535")
+    window.location.replace("https://t.me/DuskTalesBot")
 })
