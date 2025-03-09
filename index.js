91f74a78b5d2864817bfeaa25bb4bf3db3f63217
Yash <starrai843@gmail.com>
2025-03-09 15:56:22 +0530

    Event-Info

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// alert("Event Infos are now available.")
 document.getElementById("btn").addEventListener("click",()=>{
-
     window.location.replace("https://t.me/DuskTalesBot")
-
     
 })
+document.getElementById("hamburger").addEventListener("click",()=>{
+   var x=document.getElementById("menu");
+   if(x.style.display=="block"){
+     x.style.display="none";
+   }
+   else{
+   x.style.display="block";
+   }
+})
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  hamburger.classList.toggle("active"); // Add rotation effect
+});
